ffac58c4e8ce92c3271ef62744056f0864c4b7f2
Liam Hoflay <Liametc@users.noreply.github.com>
2020-04-29 12:09:04 +0100

    Explicit QIcon for Katana/PyQt (v2.3.4+wwfx.1.0.0)

--- python/tk_multi_publish2/publish_tree_widget/tree_node_item.py
+++ python/tk_multi_publish2/publish_tree_widget/tree_node_item.py
@@ -183,7 +183,7 @@ class TreeNodeItem(TreeNodeBase):
         else:
             icon = self._expanded_icon
 
-        self._embedded_widget.expand_indicator.setIcon(icon)
+        self._embedded_widget.expand_indicator.setIcon(QtGui.QIcon(icon))
 
     def _check_expand_state(self):
         """
@@ -196,7 +196,7 @@ class TreeNodeItem(TreeNodeBase):
         else:
             icon = self._collapsed_icon
 
-        self._embedded_widget.expand_indicator.setIcon(icon)
+        self._embedded_widget.expand_indicator.setIcon(QtGui.QIcon(icon))
 
 
 class TopLevelTreeNodeItem(TreeNodeItem):
